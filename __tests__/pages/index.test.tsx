a1b5173e89828f92aeee32c0c76c5ca8a3fa558f
Suhardi-14 <69751852+Suhardi-14@users.noreply.github.com>
2023-01-13 12:33:53 +0800

    Setup jest

---
+++ __tests__/pages/index.test.tsx
@@ -0,0 +1,25 @@
+import { logRoles, render, screen } from "@testing-library/react";
+import Home from "../../pages/index";
+import { useRouter } from "next/router";
+// import "@testing-library/jest-dom";
+
+jest.mock("next/router", () => ({
+  useRouter: jest.fn(),
+}));
+
+describe("Home", () => {
+  // beforeEach(() => {
+  let expectedRouterPush = jest.fn();
+  useRouter.mockReturnValue({ push: expectedRouterPush });
+  // });
+  it("renders a heading", () => {
+    const view = render(<Home />);
+
+    const heading = screen.getByRole("heading", {
+      name: /DICOM server/i,
+    });
+    logRoles(view.container);
+
+    expect(heading).toBeInTheDocument();
+  });
+});
--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,3 @@
-// jest.config.js
 const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
@@ -7,11 +6,14 @@ const createJestConfig = nextJest({
 });
 
 // Add any custom config to be passed to Jest
-/** @type {import('jest').Config} */
 const customJestConfig = {
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  // moduleNameMapper: {
+  //   // Handle module aliases (this will be automatically configured for you soon)
+  //   "^@/components/(.*)$": "<rootDir>/components/$1",
+
+  //   "^@/pages/(.*)$": "<rootDir>/pages/$1",
+  // },
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
 };
