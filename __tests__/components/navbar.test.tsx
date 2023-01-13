6ef526c182553e2ea6e562ac81b9fe993c061f08
Suhardi-14 <69751852+Suhardi-14@users.noreply.github.com>
2023-01-14 02:35:38 +0800

    mock next router for test

--- __tests__/components/navbar.test.tsx
+++ __tests__/components/navbar.test.tsx
@@ -2,7 +2,7 @@ import { render, screen, within } from "@testing-library/react";
 import Navbar from "../../components/navbar";
 
 describe("Navbar", () => {
-  test("renders correctly", () => {
+  test("renders navbar correctly", () => {
     render(<Navbar />);
     const link = screen.getByRole("link", {
       name: /your company duerr\-ndt\-logo/i,
@@ -13,5 +13,14 @@ describe("Navbar", () => {
     });
 
     expect(companyImg).toBeVisible();
+  });
+
+  test("logo links to homepage", async () => {
+    render(<Navbar />);
+    const link = screen.getByRole("link", {
+      name: /your company duerr\-ndt\-logo/i,
+    });
+    // expect(link).not.toBeEnabled();
+    expect(link).toHaveAttribute("href", "/");
   });
 });
--- __tests__/pages/index.test.tsx
+++ __tests__/pages/index.test.tsx
@@ -1,25 +1,21 @@
 import { logRoles, render, screen } from "@testing-library/react";
 import Home from "../../pages/index";
-import { useRouter } from "next/router";
-// import "@testing-library/jest-dom";
-
-jest.mock("next/router", () => ({
-  useRouter: jest.fn(),
-}));
+import { createMockRouter } from "../../test-utils/createMockRouter";
+import { RouterContext } from "next/dist/shared/lib/router-context";
 
 describe("Home", () => {
-  // beforeEach(() => {
-  let expectedRouterPush = jest.fn();
-  useRouter.mockReturnValue({ push: expectedRouterPush });
-  // });
-  it("renders a heading", () => {
-    const view = render(<Home />);
+  test("renders a heading", () => {
+    const view = render(
+      <RouterContext.Provider value={createMockRouter({})}>
+        <Home />
+      </RouterContext.Provider>
+    );
 
     const heading = screen.getByRole("heading", {
       name: /DICOM server/i,
     });
     // logRoles(view.container);
 
-    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent(/DICOM server/i);
   });
 });
